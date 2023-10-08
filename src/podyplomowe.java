f335d7f3b8d0f8f592cfc51491e08eed94f2b725
masowad <gosia_sowa@poczta.fm>
2023-10-08 16:10:12 +0200

    Initial commit

---
+++ src/podyplomowe.java
@@ -0,0 +1,17 @@
+public class podyplomowe {
+    public static void main(String[] args) {
+
+        String firstName = "Anna" ;
+        String lastName = " Pacia" ;
+        short age = 28 ;
+        char gender = 'm' ;
+        long personalIDNumber = 2354528L;
+
+        System.out.println(" Imię :" + firstName );
+        System.out.println(" Nazwisko :" + lastName );
+        System.out.println( " Age: " + age );
+        System.out.println(" Gender: " + gender);
+        System.out.println(" Personal ID number :" + personalIDNumber);
+
+    }
+}
